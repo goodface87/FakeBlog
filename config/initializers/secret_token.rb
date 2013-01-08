52dba493cb0e51c7bcc67d9afcff40d6dd3222e1
Edgar Buenrostro <Edgar Buenrostro>
2013-01-07 21:50:28 -0800

    Added the crap
    The fake blog crap.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '11bc77fde26cb355b467c645a9ed84577075c0ea6d2252b02587bc015af393d865064cca0358b37e1f5a26a24233d1dffee344e605e294c38419a3bca1b50d81'
