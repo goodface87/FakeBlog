73b66f6ed8d1525b7b3f50d9aa7187d09770c3c9
Edgar Buenrostro <Edgar Buenrostro>
2013-01-14 19:17:35 -0800

    Adds relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
-
+  has_many :comments
   validates :name,  :presence => true
   validates :title, :presence => true,
             :length => { :minimum => 5 }
